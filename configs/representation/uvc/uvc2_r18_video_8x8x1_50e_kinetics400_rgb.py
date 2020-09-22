b51d5cc5a6b54892c4065bc109925b7254e9cce5
Jiarui XU <xvjiarui0826@gmail.com>
2020-09-23 02:04:43 +0800

    fixed len

--- configs/representation/uvc/uvc2_r18_video_8x8x1_50e_kinetics400_rgb.py
+++ configs/representation/uvc/uvc2_r18_video_8x8x1_50e_kinetics400_rgb.py
@@ -54,7 +54,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 train_pipeline = [
     dict(type='DecordInit'),
-    dict(type='SampleFrames', clip_len=2, frame_interval=8, num_clips=1),
+    dict(type='SampleFrames', clip_len=8, frame_interval=8, num_clips=1),
     dict(type='DecordDecode'),
     # dict(type='Resize', scale=(-1, 256)),
     # dict(type='RandomResizedCrop'),
