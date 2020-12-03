75060af5b198f6009afb2884cb448f6060b35486
Jiarui XU <xvjiarui0826@gmail.com>
2020-12-03 12:35:40 -0800

    add davis eval

--- tools/test_seq_davis.py
+++ tools/test_seq_davis.py
@@ -1,19 +1,19 @@
 import argparse
 import os
 import os.path as osp
+import time
 
 import mmcv
 import torch
 import torch.distributed as dist
-from mmcv.cnn import fuse_conv_bn
 from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
 from mmcv.runner import get_dist_info, init_dist, load_checkpoint
-from mmcv.runner.fp16_utils import wrap_fp16_model
 from mmcv.utils import DictAction
 
 from mmaction.apis import multi_gpu_test, single_gpu_test
 from mmaction.datasets import build_dataloader, build_dataset
 from mmaction.models import build_model
+from mmaction.utils import get_root_logger
 
 
 def parse_args():
@@ -23,11 +23,6 @@ def parse_args():
     parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
         '--out', default=None, help='output result file in pickle format')
-    parser.add_argument(
-        '--fuse-conv-bn',
-        action='store_true',
-        help='Whether to fuse conv and bn, this will slightly increase'
-        'the inference speed')
     parser.add_argument(
         '--eval',
         type=str,
@@ -40,6 +35,7 @@ def parse_args():
         help='whether to use gpu to collect results')
     parser.add_argument(
         '--tmpdir',
+        default='tmpdir',
         help='tmp directory used for collecting results from multiple '
         'workers, available when gpu-collect is not specified')
     parser.add_argument(
@@ -49,11 +45,9 @@ def parse_args():
         nargs='+',
         action=DictAction,
         help='custom evaluation options')
-    parser.add_argument(
-        '--average-clips',
-        choices=['score', 'prob'],
-        default='score',
-        help='average type when averaging test clips')
+    parser.add_argument('--start-epoch', type=int, default=1)
+    parser.add_argument('--eval-interval', type=int, default=1)
+    parser.add_argument('--skip', action='store_true')
     parser.add_argument(
         '--launcher',
         choices=['none', 'pytorch', 'slurm', 'mpi'],
@@ -93,14 +87,10 @@ def main():
         # use config filename as default work_dir if cfg.work_dir is None
         cfg.work_dir = osp.join('./work_dirs',
                                 osp.splitext(osp.basename(args.config))[0])
-    wandb = None
-    for h in cfg.log_config.hooks:
-        if h.type == 'WandbLoggerHook':
-            import wandb
-            wandb.init(**h.init_kwargs.to_dict())
-            mmcv.mkdir_or_exist(
-                osp.join('./wandb',
-                         osp.splitext(osp.basename(args.config))[0]))
+    # init logger before other steps
+    timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    log_file = osp.join(cfg.work_dir, f'{timestamp}.log')
+    logger = get_root_logger(log_file=log_file, log_level=cfg.log_level)
 
     # Load output_config from cfg
     output_config = cfg.get('output_config', {})
@@ -123,20 +113,25 @@ def main():
         torch.backends.cudnn.benchmark = True
     cfg.data.test.test_mode = True
 
-    if cfg.test_cfg is None:
-        cfg.test_cfg = dict(average_clips=args.average_clips)
-    else:
-        cfg.test_cfg.average_clips = args.average_clips
-
     # init distributed env first, since logger depends on the dist info.
     if args.launcher == 'none':
         distributed = False
     else:
         distributed = True
         init_dist(args.launcher, **cfg.dist_params)
+    rank, world_size = get_dist_info()
+
+    wandb = None
+    for h in cfg.log_config.hooks:
+        if rank == 0 and h.type == 'WandbLoggerHook':
+            import wandb
+            h.init_kwargs.name = f'dv-{h.init_kwargs.name}'
+            h.init_kwargs.tags = [*h.init_kwargs.tags, 'davis']
+            wandb.init(**h.init_kwargs.to_dict())
 
     # build the dataloader
     dataset = build_dataset(cfg.data.test, dict(test_mode=True))
+    train_len = len(build_dataset(cfg.data.train))
     data_loader = build_dataloader(
         dataset,
         videos_per_gpu=1,
@@ -146,17 +141,25 @@ def main():
 
     # build the model and load checkpoint
     model = build_model(cfg.model, train_cfg=None, test_cfg=cfg.test_cfg)
-    fp16_cfg = cfg.get('fp16', None)
-    if fp16_cfg is not None:
-        wrap_fp16_model(model)
-    for epoch in range(cfg.total_epochs):
-        ckpt_path = osp.join(cfg.work_dir, f'epoch_{epoch+1}.pth')
+    train_iters = train_len // world_size // cfg.data.videos_per_gpu
+    for epoch in range(args.start_epoch, cfg.total_epochs + 1,
+                       cfg.checkpoint_config.interval):
+        if epoch % args.eval_interval != 0:
+            continue
+        ckpt_path = osp.join(cfg.work_dir, f'epoch_{epoch}.pth')
         if not osp.exists(ckpt_path):
-            print(f'skping {epoch+1}')
-        load_checkpoint(model, ckpt_path, map_location='cpu')
+            if args.skip:
+                logger.info(f'{ckpt_path} not exist, skipping')
+                continue
+            else:
+                logger.info(f'{ckpt_path} not exist, waiting')
+        while not osp.exists(ckpt_path):
+            time.sleep(300)
+        logger.info(f'Found {ckpt_path}')
 
-        if args.fuse_conv_bn:
-            model = fuse_conv_bn(model)
+        load_checkpoint(model, ckpt_path, map_location='cpu')
+        if distributed:
+            dist.barrier()
 
         if not distributed:
             model = MMDataParallel(model, device_ids=[0])
@@ -169,18 +172,18 @@ def main():
             outputs = multi_gpu_test(model, data_loader, args.tmpdir,
                                      args.gpu_collect)
 
-        rank, _ = get_dist_info()
         if rank == 0:
             if output_config:
                 out = output_config['out']
-                print(f'\nwriting results to {out}')
+                logger.info(f'\nwriting results to {out}')
                 dataset.dump_results(outputs, **output_config)
             if eval_config:
-                eval_res = dataset.evaluate(outputs, **eval_config)
+                eval_res = dataset.evaluate(
+                    outputs, **eval_config, logger=logger)
                 for name, val in eval_res.items():
-                    print(f'{name}: {val:.04f}')
+                    logger.info(f'{name}: {val:.04f}')
                 if wandb is not None:
-                    wandb.log(eval_res, step=epoch + 1)
+                    wandb.log(eval_res, step=epoch * train_iters, commit=False)
         if distributed:
             dist.barrier()
 
