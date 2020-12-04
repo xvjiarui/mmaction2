b4f9ff7c2d59e9433293a23ebb1d04269905efef
Jiarui XU <xvjiarui0826@gmail.com>
2020-12-04 01:11:15 -0800

    fixed grid

--- configs/representation/sim_siam_pix/sim_siam_pix_r18_c4_sgd_cosine_50e_imagenet.py
+++ configs/representation/sim_siam_pix/sim_siam_pix_r18_c4_sgd_cosine_50e_imagenet.py
@@ -84,6 +84,7 @@ train_pipeline = [
     dict(type='SampleFrames', clip_len=1, frame_interval=8, num_clips=1),
     dict(type='DuplicateFrames', times=2),
     dict(type='RawImageDecode'),
+    dict(type='Grid'),
     dict(
         type='RandomResizedCrop',
         area_range=(0.2, 1.),
@@ -116,8 +117,10 @@ train_pipeline = [
         same_on_clip=False),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW'),
-    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
-    dict(type='ToTensor', keys=['imgs', 'label'])
+    # dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    # dict(type='ToTensor', keys=['imgs', 'label'])
+    dict(type='Collect', keys=['imgs', 'grids'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs', 'grids'])
 ]
 val_pipeline = [
     dict(type='SequentialSampleFrames', frame_interval=1),
