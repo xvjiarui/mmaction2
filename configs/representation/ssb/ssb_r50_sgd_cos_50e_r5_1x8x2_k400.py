4d75d50e305072fbe2e328610761e408dec36554
Jiarui XU <xvjiarui0826@gmail.com>
2021-01-04 23:18:03 -0800

    add longer r50

--- configs/representation/ssb/ssb_r50_sgd_cos_50e_r5_1x8x2_k400.py
+++ configs/representation/ssb/ssb_r50_sgd_cos_50e_r5_1x8x2_k400.py
@@ -1,15 +1,12 @@
 # model settings
-temperature = 0.2
-with_norm = True
-query_dim = 128
 model = dict(
     type='SimSiamBaseTracker',
     backbone=dict(
         type='ResNet',
         pretrained=None,
-        depth=18,
+        depth=50,
         out_indices=(3, ),
-        strides=(1, 2, 2, 1),
+        # strides=(1, 2, 1, 1),
         norm_cfg=dict(type='SyncBN', requires_grad=True),
         norm_eval=False,
         zero_init_residual=True),
@@ -17,20 +14,19 @@ model = dict(
     # patch_head=None,
     img_head=dict(
         type='SimSiamHead',
-        in_channels=512,
+        in_channels=2048,
         norm_cfg=dict(type='SyncBN'),
-        drop_layer_cfg=dict(type='Dropout2d', p=0.5, inplace=True),
         num_projection_fcs=3,
-        projection_mid_channels=512,
-        projection_out_channels=512,
+        projection_mid_channels=2048,
+        projection_out_channels=2048,
         num_predictor_fcs=2,
-        predictor_mid_channels=128,
-        predictor_out_channels=512,
+        predictor_mid_channels=512,
+        predictor_out_channels=2048,
         with_norm=True,
         loss_feat=dict(type='CosineSimLoss', negative=False),
         spatial_type='avg'))
 # model training and testing settings
-train_cfg = dict(intra_video=False, image2patch=False)
+train_cfg = dict(intra_video=False)
 test_cfg = dict(
     precede_frames=20,
     topk=10,
@@ -57,47 +53,37 @@ train_pipeline = [
     dict(type='SampleFrames', clip_len=1, frame_interval=8, num_clips=2),
     # dict(type='DuplicateFrames', times=2),
     dict(type='DecordDecode'),
-    # dict(
-    #     type='RandomResizedCrop',
-    #     area_range=(0.2, 1.),
-    #     same_across_clip=False,
-    #     same_on_clip=False),
-    dict(type='Resize', scale=(256, 256), keep_ratio=False),
-    # dict(type='RandomAffine',
-    #      degrees=10,
-    #      p=0.5,
-    #      shear=(-0.1, 0.1, -0.1, 0.1),
-    #      same_across_clip=False,
-    #      same_on_clip=False),
-    # dict(
-    #     type='Flip',
-    #     flip_ratio=0.5,
-    #     same_across_clip=False,
-    #     same_on_clip=False),
-    # dict(
-    #     type='ColorJitter',
-    #     brightness=0.4,
-    #     contrast=0.4,
-    #     saturation=0.4,
-    #     hue=0.1,
-    #     p=0.8,
-    #     same_across_clip=False,
-    #     same_on_clip=False),
-    # dict(
-    #     type='RandomGrayScale',
-    #     p=0.2,
-    #     same_across_clip=False,
-    #     same_on_clip=False),
-    # dict(
-    #     type='RandomGaussianBlur',
-    #     p=0.5,
-    #     same_across_clip=False,
-    #     same_on_clip=False),
-    # dict(type='RandomChoiceRotate', p=1, degrees=(0, 90, 180, 270),
-    #      same_on_clip=False, same_across_clip=False),
+    dict(
+        type='RandomResizedCrop',
+        area_range=(0.2, 1.),
+        same_across_clip=False,
+        same_on_clip=False),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(
+        type='Flip',
+        flip_ratio=0.5,
+        same_across_clip=False,
+        same_on_clip=False),
+    dict(
+        type='ColorJitter',
+        brightness=0.4,
+        contrast=0.4,
+        saturation=0.4,
+        hue=0.1,
+        p=0.8,
+        same_across_clip=False,
+        same_on_clip=False),
+    dict(
+        type='RandomGrayScale',
+        p=0.2,
+        same_across_clip=False,
+        same_on_clip=False),
+    dict(
+        type='RandomGaussianBlur',
+        p=0.5,
+        same_across_clip=False,
+        same_on_clip=False),
     dict(type='Normalize', **img_norm_cfg),
-    # dict(type='HidePatch', patch_size=[16, 32, 44, 56], hide_prob=0.2),
-    dict(type='RandomErasing', p=0.2, mode='pixel'),
     dict(type='FormatShape', input_format='NCTHW'),
     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
     dict(type='ToTensor', keys=['imgs', 'label'])
@@ -116,14 +102,17 @@ val_pipeline = [
     dict(type='ToTensor', keys=['imgs', 'ref_seg_map'])
 ]
 data = dict(
-    videos_per_gpu=128,
-    workers_per_gpu=16,
+    videos_per_gpu=32,
+    workers_per_gpu=4,
     val_workers_per_gpu=1,
     train=dict(
-        type=dataset_type,
-        ann_file=ann_file_train,
-        data_prefix=data_prefix,
-        pipeline=train_pipeline),
+        type='RepeatDataset',
+        times=5,
+        dataset=dict(
+            type=dataset_type,
+            ann_file=ann_file_train,
+            data_prefix=data_prefix,
+            pipeline=train_pipeline)),
     val=dict(
         type=dataset_type_val,
         ann_file=ann_file_val,
@@ -161,23 +150,23 @@ evaluation = dict(
     key_indicator='feat_1.J&F-Mean',
     rule='greater')
 log_config = dict(
-    interval=10,
+    interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
         # dict(type='TensorboardLoggerHook'),
-        # dict(
-        #     type='WandbLoggerHook',
-        #     init_kwargs=dict(
-        #         project='mmaction2',
-        #         name='{{fileBasenameNoExtension}}',
-        #         resume=True,
-        #         tags=['moco2'],
-        #         dir='wandb/{{fileBasenameNoExtension}}',
-        #         config=dict(
-        #             model=model,
-        #             train_cfg=train_cfg,
-        #             test_cfg=test_cfg,
-        #             data=data))),
+        dict(
+            type='WandbLoggerHook',
+            init_kwargs=dict(
+                project='mmaction2',
+                name='{{fileBasenameNoExtension}}',
+                resume=True,
+                tags=['ssb'],
+                dir='wandb/{{fileBasenameNoExtension}}',
+                config=dict(
+                    model=model,
+                    train_cfg=train_cfg,
+                    test_cfg=test_cfg,
+                    data=data))),
     ])
 # runtime settings
 dist_params = dict(backend='nccl')
